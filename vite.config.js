c4874119754dae5b2b13d866c457bf65fa4665a1
ROTEMIL95 <ROTEMILUZ53@GMAIL.com>
2025-04-07 18:33:02 +0300

    Add PWA support: integrate vite-plugin-pwa for improved offline capabilities and update package dependencies in package.json and package-lock.json.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import viteCompression from "vite-plugin-compression"; // ← הוספה כאן
+import viteCompression from "vite-plugin-compression";
 import removeConsole from "vite-plugin-remove-console";
 import { visualizer } from "rollup-plugin-visualizer";
-
+import { VitePWA } from "vite-plugin-pwa"; // ✅ נוספה כאן
 
 export default defineConfig({
-  base: "/", // for github pages
+  base: "/",
   plugins: [
     react(),
-    viteCompression(), 
+    viteCompression(),
     removeConsole({ exclude: ['error'] }),
     visualizer({
       filename: "./dist/bundle-report.html",
       gzipSize: true,
       brotliSize: true,
       open: true
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Sitonim-il",
+        short_name: "Sitonimil",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1e88e5",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/icons/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
     })
   ],
   server: {
